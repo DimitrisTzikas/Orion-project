cbcbe5a91b9c26767016656373780676a5b8552e
Dimitris Tzikas <dimitristzikas@protonmail.com>
2019-12-15 17:27:44 +0200

    deleting old files

--- src/Enum/AnnouncementType.java
+++
@@ -1,7 +0,0 @@
-package Enum;
-
-public enum AnnouncementType {
-    
-    Exam1,Exam2,Exam3,Exam4,Exam5,Exam6,Exam7,Exam8,Exam9,Exam10,General
-    
-}
--- src/Enum/CourseType.java
+++
@@ -1,7 +0,0 @@
-package Enum;
-
-public enum CourseType {
-    
-    Compulsory, Optional
-    
-}
